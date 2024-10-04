78e27f2cd7e3ffd74c5cec87b9aaf6c95bdb3b75
Tearever <trevor.wright17@yahoo.com>
2024-10-04 11:47:32 -0500

    Added victory noise for completing level

--- Assets/__Scripts/Goal.cs
+++ Assets/__Scripts/Goal.cs
@@ -6,8 +6,20 @@ using UnityEngine;
 public class Goal : MonoBehaviour
 {
     static public bool goalMet = false;
+    public AudioClip goalSound; 
+    private AudioSource audioSource;
 
-    void OnTriggerEnter(Collider other){
+    void Start() 
+    {
+        audioSource = GetComponent<AudioSource>();
+            if (audioSource == null) {
+                audioSource = gameObject.AddComponent<AudioSource>();
+            }
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
         //When the trigger is hit in the cube goal object
         //Checks to see if it was the Projectile
         Projectile proj = other.GetComponent<Projectile>();
@@ -19,6 +31,9 @@ public class Goal : MonoBehaviour
             Color c = mat.color;
             c.a = 0.75f;
             mat.color = c;
+
+            //Add Audio Component
+            audioSource.PlayOneShot(goalSound);
 
         }
     }
